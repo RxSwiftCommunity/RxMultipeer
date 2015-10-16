c94718466e6fc9749ef1cbc08da8f9ba4d22c9d5
Nathan Kot <me@nathankot.com>
2015-10-16 12:46:34 +0900

    Re-organize and re-factor

--- NKMultipeer/Adapters/MockSession.swift
+++ NKMultipeer/Adapters/MockSession.swift
@@ -63,13 +63,10 @@ public class MockSession : Session {
   // Connection concerns
   //////////////////////////////////////////////////////////////////////////
 
-  var _connections: [Weak<MockSession>] = [] {
-    didSet {
-      rx_connections.value = _connections
-        .filter { $0.value != nil }
-        .map { $0.value!.iden }
-    }
-  }
+  let rx_connections = Variable<[Weak<MockSession>]>([])
+  let rx_connectedPeer = PublishSubject<I>()
+  let rx_disconnectedPeer = PublishSubject<I>()
+  let rx_connectRequests = PublishSubject<(I, [String: AnyObject]?, (Bool) -> ())>()
 
   var isAdvertising = false {
     didSet { MockSession.digest() }
@@ -79,24 +76,17 @@ public class MockSession : Session {
     didSet { MockSession.digest() }
   }
 
-  let connectRequests: PublishSubject<(I, [String: AnyObject]?, (Bool) -> ())> = PublishSubject()
-
-  let rx_connectedPeer: PublishSubject<I> = PublishSubject()
-
   public func connectedPeer() -> Observable<I> {
     return rx_connectedPeer
   }
-
-  let rx_disconnectedPeer: PublishSubject<I> = PublishSubject()
 
   public func disconnectedPeer() -> Observable<I> {
     return rx_disconnectedPeer
   }
 
-  private let rx_connections = Variable<[I]>([])
-
   public func connections() -> Observable<[I]> {
     return rx_connections.asObservable()
+      .map { $0.filter { $0.value != nil }.map { $0.value!.iden } }
   }
 
   public func nearbyPeers() -> Observable<[(I, [String: String]?)]> {
@@ -106,7 +96,7 @@ public class MockSession : Session {
   }
 
   public func incomingConnections() -> Observable<(I, [String: AnyObject]?, (Bool) -> ())> {
-    return connectRequests.filter { _ in self.isAdvertising }
+    return rx_connectRequests.filter { _ in self.isAdvertising }
   }
 
   public func startBrowsing() {
@@ -129,32 +119,32 @@ public class MockSession : Session {
     let otherm = MockSession.sessions.filter({ return $0.iden == peer }).first
     if let other = otherm {
       // Skip if already connected
-      if self._connections.filter({ $0.value?.iden == other.iden }).count > 0 { return }
+      if self.rx_connections.value.filter({ $0.value?.iden == other.iden }).count > 0 {
+        return
+      }
 
       if other.isAdvertising {
-        other.connectRequests.on(.Next(
+        other.rx_connectRequests.on(.Next(
           (self.iden,
             context,
-            { [weak self] (response: Bool) in
+            { [unowned self] (response: Bool) in
               if !response { return }
-              if let this = self {
-                this._connections.append(Weak(other))
-                other._connections.append(Weak(this))
-                this.rx_connectedPeer.on(.Next(other.iden))
-                other.rx_connectedPeer.on(.Next(this.iden))
-              }
+              self.rx_connections.value = self.rx_connections.value + [Weak(other)]
+              other.rx_connections.value = other.rx_connections.value + [Weak(self)]
+              self.rx_connectedPeer.on(.Next(other.iden))
+              other.rx_connectedPeer.on(.Next(self.iden))
             }) as (I, [String: AnyObject]?, (Bool) -> ())))
       }
     }
   }
 
   public func disconnect() {
-    self._connections = []
+    self.rx_connections.value = []
     MockSession.sessions = MockSession.sessions.filter { $0.iden != self.iden }
     for session in MockSession.sessions {
-      let old = session._connections
-      session._connections = session._connections.filter { $0.value?.iden != self.iden }
-      if old.count > session._connections.count {
+      let old = session.rx_connections.value
+      session.rx_connections.value = session.rx_connections.value.filter { $0.value?.iden != self.iden }
+      if old.count > session.rx_connections.value.count {
         session.rx_disconnectedPeer.on(.Next(self.iden))
       }
     }
@@ -167,22 +157,22 @@ public class MockSession : Session {
   // Data reception concerns
   //////////////////////////////////////////////////////////////////////////
 
-  let receivedData: PublishSubject<(I, NSData)> = PublishSubject()
-  let receivedResources: PublishSubject<(I, String, ResourceState)> = PublishSubject()
+  let rx_receivedData = PublishSubject<(I, NSData)>()
+  let rx_receivedResources = PublishSubject<(I, String, ResourceState)>()
 
   public func receive() -> Observable<(I, NSData)> {
-    return receivedData
+    return rx_receivedData
   }
 
   public func receive() -> Observable<(I, String, ResourceState)> {
-    return receivedResources
+    return rx_receivedResources
   }
 
   // Data delivery concerns
   //////////////////////////////////////////////////////////////////////////
 
   func isConnected(other: MockSession) -> Bool {
-    return _connections.filter({ $0.value?.iden == other.iden }).first != nil
+    return rx_connections.value.filter({ $0.value?.iden == other.iden }).first != nil
   }
 
   public func send
@@ -190,13 +180,13 @@ public class MockSession : Session {
    _ data: NSData,
    _ mode: MCSessionSendDataMode)
   -> Observable<()> {
-    return create { observer in
+    return create { [unowned self] observer in
       if let otherSession = MockSession.findForClient(other) {
         // Can't send if not connected
         if !self.isConnected(otherSession) {
           observer.on(.Error(NKMultipeerError.ConnectionError))
         } else {
-          otherSession.receivedData.on(.Next((self.iden, data)))
+          otherSession.rx_receivedData.on(.Next((self.iden, data)))
           observer.on(.Next(()))
           observer.on(.Completed)
         }
@@ -214,15 +204,15 @@ public class MockSession : Session {
    url: NSURL,
    _ mode: MCSessionSendDataMode)
   -> Observable<(NSProgress)> {
-    return create { observer in
+    return create { [unowned self] observer in
       if let otherSession = MockSession.findForClient(other) {
         // Can't send if not connected
         if !self.isConnected(otherSession) {
           observer.on(.Error(NKMultipeerError.ConnectionError))
         } else {
           let c = self.iden
-          otherSession.receivedResources.on(.Next(c, name, .Progress(NSProgress(totalUnitCount: 1))))
-          otherSession.receivedResources.on(.Next(c, name, .Finished(url)))
+          otherSession.rx_receivedResources.on(.Next(c, name, .Progress(NSProgress(totalUnitCount: 1))))
+          otherSession.rx_receivedResources.on(.Next(c, name, .Finished(url)))
           let completed = NSProgress(totalUnitCount: 1)
           completed.completedUnitCount = 1
           observer.on(.Next(completed))
--- NKMultipeerTests/IntegrationSpec.swift
+++ NKMultipeerTests/IntegrationSpec.swift
@@ -11,14 +11,12 @@ public class IntegrationSpec : QuickSpec {
 
     describe("two clients") {
 
-      var clientone: CurrentClient<MockIden, MockSession>?
-      var clienttwo: CurrentClient<MockIden, MockSession>?
+      var clientone: CurrentClient<MockIden, MockSession>!
+      var clienttwo: CurrentClient<MockIden, MockSession>!
 
       beforeEach {
-        disposeBag = DisposeBag()
-        clientone = nil
-        clienttwo = nil
         MockSession.reset()
+        disposeBag = DisposeBag()
         clientone = CurrentClient(session: MockSession(name: "one"))
         clienttwo = CurrentClient(session: MockSession(name: "two"))
       }
@@ -27,8 +25,8 @@ public class IntegrationSpec : QuickSpec {
 
         beforeEach {
           // Advertise and always accept connections
-          clienttwo!.startAdvertising()
-          clienttwo!.incomingConnections()
+          clienttwo.startAdvertising()
+          clienttwo.incomingConnections()
           .subscribeNext { (client, _, respond) in
             respond(true) }
           .addDisposableTo(disposeBag)
@@ -36,39 +34,37 @@ public class IntegrationSpec : QuickSpec {
 
         it("allows client one to browse for client two") {
           waitUntil { done in
-            clientone!.startBrowsing()
-            clientone!.nearbyPeers()
+            clientone.startBrowsing()
+            clientone.nearbyPeers()
             .filter { $0.count > 0 }
             .subscribeNext { clients in
-              expect(clients[0].0.iden).to(equal(clienttwo!.iden))
+              expect(clients[0].0.iden).to(equal(clienttwo.iden))
               done()
             }
             .addDisposableTo(disposeBag)
-            return
           }
-          return
         }
 
         it("allows clients to connect with eachother") {
           waitUntil { done in
-            clientone!.connectedPeer()
+            clientone.connectedPeer()
             .take(1)
             .subscribeNext { _ in done() }
             .addDisposableTo(disposeBag)
 
-            clientone!.connect(clienttwo!)
+            clientone.connect(clienttwo)
           }
         }
 
         it("alters connections when clients connect") {
           waitUntil { done in
             combineLatest(
-              clientone!.connections(),
-              clienttwo!.connections()) { $0.count + $1.count }
+              clientone.connections(),
+              clienttwo.connections()) { $0.count + $1.count }
             .subscribeNext { if $0 == 2 { done() } }
             .addDisposableTo(disposeBag)
 
-            clientone!.connect(clienttwo!)
+            clientone.connect(clienttwo)
           }
         }
 
@@ -76,20 +72,20 @@ public class IntegrationSpec : QuickSpec {
 
           beforeEach {
             waitUntil { done in
-              clientone!.connectedPeer()
+              clientone.connectedPeer()
               .take(1)
               .subscribeNext { _ in done() }
               .addDisposableTo(disposeBag)
 
-              clientone!.connect(clienttwo!)
+              clientone.connect(clienttwo)
             }
           }
 
           it("allows client two to disconnect") {
             waitUntil { done in
               combineLatest(
-                clientone!.disconnectedPeer(),
-                clientone!.connections()) { ($0, $1) }
+                clientone.disconnectedPeer(),
+                clientone.connections()) { ($0, $1) }
               .take(1)
               .subscribeNext { (_, connections) in
                 expect(connections.count).to(equal(0))
@@ -97,13 +93,13 @@ public class IntegrationSpec : QuickSpec {
               }
               .addDisposableTo(disposeBag)
 
-              clienttwo!.disconnect()
+              clienttwo.disconnect()
             }
           }
 
           it("fires a next event when sending data") {
             waitUntil { done in
-              clientone!.send(clienttwo!, "hello")
+              clientone.send(clienttwo, "hello")
               .subscribeNext { _ in done() }
               .addDisposableTo(disposeBag)
             }
@@ -111,14 +107,14 @@ public class IntegrationSpec : QuickSpec {
 
           it("lets clients send strings to eachother") {
             waitUntil { done in
-              clienttwo!.receive()
+              clienttwo.receive()
               .subscribeNext { (client: Client, string: String) in
-                expect(client.iden).to(equal(clientone!.iden))
+                expect(client.iden).to(equal(clientone.iden))
                 expect(string).to(equal("hello"))
               }
               .addDisposableTo(disposeBag)
 
-              clientone!.send(clienttwo!, "hello")
+              clientone.send(clienttwo, "hello")
               .subscribeCompleted { done() }
               .addDisposableTo(disposeBag)
             }
@@ -126,9 +122,9 @@ public class IntegrationSpec : QuickSpec {
 
           it("lets clients send resource urls to each other") {
             waitUntil { done in
-              clienttwo!.receive()
+              clienttwo.receive()
               .subscribeNext { (client: Client, name: String, url: NSURL) in
-                expect(client.iden).to(equal(clientone!.iden))
+                expect(client.iden).to(equal(clientone.iden))
                 expect(name).to(equal("txt file"))
                 let contents = NSString(data: NSData(contentsOfURL: url)!, encoding: NSUTF8StringEncoding)
                 expect(contents).to(equal("hello there this is random data"))
@@ -136,7 +132,7 @@ public class IntegrationSpec : QuickSpec {
               .addDisposableTo(disposeBag)
 
               let url = NSBundle(forClass: self.dynamicType).URLForResource("Data", withExtension: "txt")
-              clientone!.send(clienttwo!, name: "txt file", url: url!)
+              clientone.send(clienttwo, name: "txt file", url: url!)
               .subscribeCompleted { done() }
               .addDisposableTo(disposeBag)
             }
@@ -144,16 +140,16 @@ public class IntegrationSpec : QuickSpec {
 
           it("lets clients send JSON data to each other via foundation objects") {
             waitUntil { done in
-              clienttwo!.receive()
+              clienttwo.receive()
               .subscribeNext { (client: Client, json: [String: AnyObject]) in
-                expect(client.iden).to(equal(clientone!.iden))
+                expect(client.iden).to(equal(clientone.iden))
                 expect(json["one"] as? String).to(equal("two"))
                 expect(json["three"] as? Int).to(equal(4))
                 expect(json["five"] as? Bool).to(beTrue())
               }
               .addDisposableTo(disposeBag)
 
-              clientone!.send(clienttwo!, [
+              clientone.send(clienttwo, [
                 "one": "two", "three": 4, "five": true])
               .subscribeCompleted { done() }
               .addDisposableTo(disposeBag)
